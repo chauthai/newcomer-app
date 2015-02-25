9c994da60f20aaf7ef9538908b474c4030d0ce40
Chau Thai <chau.thai@gmail.com>
2015-02-25 10:10:38 +0100

    fix infinite loop bug in home screen

--- www/js/app.js
+++ www/js/app.js
@@ -38,16 +38,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
   .state('tab.home', {
     url: '/home',
     views: {
@@ -147,6 +137,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/home');
 
 });
